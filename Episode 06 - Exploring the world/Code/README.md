1ff059301b10401dbaec40a72a419ca29bc99f70
Canzova <nihalsingh2082002@gmail.com>
2024-02-18 21:03:28 +0530

    Episode 06

---
+++ Episode 06 - Exploring the world/Code/README.md
@@ -0,0 +1,9 @@
+# Episode 06 - Exploring the world
+
+## Why onClick={update()} won't work ??
+
+- In JavaScript and React, when you use `onClick={update()}`, it means that the `update` function is immediately invoked when the component renders. This is not what you typically want for event handlers in React.
+
+- When you use `onClick={update}`, you are passing a reference to the `update` function, and it will be called only when the actual click event occurs. This is the correct way to set up event handlers in React.
+
+- If you use `onClick={update()}`, it results in the function being called immediately when the component renders, which is not the intended behavior for handling a click event. This is why it's important to omit the parentheses when passing a function as a handler to events in React.
---
+++ Episode 06 - Exploring the world/Code/src/components/Body.js
@@ -0,0 +1,32 @@
+import RestCard from "./RestCard";
+import restList from "../utils/mockData";
+import { useState } from "react";
+
+const Body = () => {
+  // State Varibale
+  const [topRestaurants, setTopRestaurants] = useState(restList);
+
+  // ------------- Filtering the top rated restraurants---------------
+  const update = () => {
+    const filterList = topRestaurants.filter((Restaurants) => {
+      return Restaurants.info.avgRating >= 4.5;
+    });
+    // Updating the state variable
+    setTopRestaurants(filterList);
+  };
+
+  return (
+    <div className="body">
+      <div className="search" onClick={update}>
+        <button>Top Rated Restaurants</button>
+      </div>
+      <div className="rest-container">
+        {topRestaurants.map((resturant) => {
+          return <RestCard key={resturant.info?.id} restInfo={resturant} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Body;
