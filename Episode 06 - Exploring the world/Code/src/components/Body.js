3edc95bd4d8ec7ab04f579b6be988c239060248d
Canzova <nihalsingh2082002@gmail.com>
2024-02-22 20:35:26 +0530

    Code Completed

--- Episode 06 - Exploring the world/Code/src/components/Body.js
+++ Episode 06 - Exploring the world/Code/src/components/Body.js
@@ -1,12 +1,25 @@
 import RestCard from "./RestCard";
 import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
+import React from "react";
+import NotFound from "./NotFound";
 
 const Body = () => {
   // State Varibale
   const [topRestaurants, setTopRestaurants] = useState([]);
+  const arr = useState("Top Rated Restaurants");
+  const btnText = arr[0];
+  const setBtnText = arr[1];
+  const [searchText, setsearchText] = useState("");
+  // Below state varible will always conatin the actual data which we are getting from the out API
+  const [topRestaurants2, setTopRestaurants2] = useState([]);
+  const [isSearchNotFound, setIsSearchNotFound] = useState(false);
 
-// useEffect
+  /**
+   *! When ever there is a change in state varible the component gets re-rendered
+   */
+
+  // useEffect
   useEffect(() => {
     fetchData();
   }, []);
@@ -18,40 +31,91 @@ const Body = () => {
     );
     const JSON = await data.json();
 
-     console.log(JSON?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    console.log(
+      JSON?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+
     setTopRestaurants(
+      JSON?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setTopRestaurants2(
       JSON?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
   };
 
   // ------------- Filtering the top rated restraurants---------------
   const update = () => {
-    const filterList = topRestaurants.filter((Restaurants) => {
-      return Restaurants.info.avgRating >= 4.2;
+    const filterList = topRestaurants2.filter((Restaurants) => {
+      return Restaurants.info.avgRating >= 4.4;
     });
-    // Updating the state variable
-    setTopRestaurants(filterList);
+
+    if (btnText === "Top Rated Restaurants") {
+      setBtnText("Back");
+      // Updating the state variable
+      setTopRestaurants(filterList);
+    } else {
+      setBtnText("Top Rated Restaurants");
+      // Fetching data again to get un-filtered list of restraunts
+      setTopRestaurants(topRestaurants2);
+    }
   };
 
   // ------------- Shimmer UI & Conditional Rendering---------------
-  if (topRestaurants.length === 0) {
+  if (topRestaurants.length === 0 && !isSearchNotFound) {
     // Because topRestaurants is an array
     return (
-      <div className="shimmer-container">
-        {new Array(8).fill(undefined).map((ele,index) => {
-          return <Shimmer key={index} />;
-        })}
-      </div>
+      <React.Fragment>
+        <div className="shimmer-container">
+          {new Array(8).fill(undefined).map((ele, index) => {
+            return <Shimmer key={index} />;
+          })}
+        </div>
+      </React.Fragment>
     );
   }
 
-  return (
+  return isSearchNotFound ? (
+    <NotFound />
+  ) : (
     <div className="body">
       {/* <div className="search" onClick={()=>{
         update();
       }}></div> */}
-      <div className="search" onClick={update}>
-        <button>Top Rated Restaurants</button>
+      <div className="search">
+        <div className="search-ele">
+          <input
+            type="text"
+            className="searchBox"
+            value={searchText}
+            onChange={(text) => {
+              setsearchText(text.target.value);
+            }}
+          />
+          <button
+            className="login-btn"
+            onClick={() => {
+              // -------------------------Filtering the data on serach---------------------------
+              const filteredData = topRestaurants2.filter((restaurant) => {
+                return restaurant?.info?.name
+                  .toLowerCase()
+                  .includes(searchText.toLowerCase());
+              });
+              filteredData.length === 0
+                ? setIsSearchNotFound(true)
+                : setIsSearchNotFound(false);
+              setTopRestaurants(filteredData);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <button
+          className="login-btn"
+          onClick={update}
+          style={{ width: btnText === "Back" ? "6rem" : "13rem" }}
+        >
+          {btnText}
+        </button>
       </div>
       <div className="rest-container">
         {topRestaurants.map((resturant) => {
