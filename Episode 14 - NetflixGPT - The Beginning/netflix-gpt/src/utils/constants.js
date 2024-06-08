f275b851f1ee2991190e9f362ef5575a8bddbc5b
Canzova <nihalsingh2082002@gmail.com>
2024-06-08 21:05:44 +0530

    Added GPT Page

--- Episode 14 - NetflixGPT - The Beginning/netflix-gpt/src/utils/constants.js
+++ Episode 14 - NetflixGPT - The Beginning/netflix-gpt/src/utils/constants.js
@@ -19,4 +19,4 @@ export const MOVIE_POSTER_URL = "https://image.tmdb.org/t/p/w500";
 export const BG_IMG =
   "https://assets.nflxext.com/ffe/siteui/vlv3/9d3533b2-0e2b-40b2-95e0-ecd7979cc88b/a3873901-5b7c-46eb-b9fa-12fea5197bd3/IN-en-20240311-popsignuptwoweeks-perspective_alpha_website_medium.jpg";
 
-export const GPT_KEY = "sk-";
+export const GPT_KEY = "";
