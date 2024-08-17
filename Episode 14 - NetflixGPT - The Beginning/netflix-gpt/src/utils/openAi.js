6d38dbb0eb26e0b0e7a02da529c25e92ba850db9
Canzova <nihalsingh2082002@gmail.com>
2024-08-17 15:38:33 +0530

    Started namaste yt

--- Episode 14 - NetflixGPT - The Beginning/netflix-gpt/src/utils/openAi.js
+++
@@ -1,7 +0,0 @@
-import OpenAI from "openai";
-import { GPT_KEY } from "../utils/constants";
-const openAi = new OpenAI({
-  apiKey: GPT_KEY, // This is the default and can be omitted
-  dangerouslyAllowBrowser: true,
-});
-export default openAi;
